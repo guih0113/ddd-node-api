c2927d2e1ff3b04ee4f7ae77362693f12871ad00
guih0113 <gui.henriquess13@gmail.com>
2026-02-22 20:04:15 -0300

    chore: create a base class for IDs

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,13 +1,21 @@
 import { expect, it } from 'vitest'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-it('should be able to answer a question', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  }
+}
 
-  const answer = answerQuestion.execute({
+it('should be able to answer a question', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
-    content: 'New answer',
+    content: 'New answer'
   })
 
   expect(answer.content).toEqual('New answer')
