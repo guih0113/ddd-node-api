afabf971cafeac0f06c88329f3fe9e456791bd58
guih0113 <gui.henriquess13@gmail.com>
2026-02-23 12:51:13 -0300

    chore: add methods "create" in all classes and create optional type

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,28 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityId } from '../../core/entities/unique-entity-id'
+import { Optional } from '../../core/types/optional'
 import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityId) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date()
+      },
+      id
+    )
+
+    return question
+  }
+}
