57ada29e34d50523a330f4faf317191b1351a698
guih0113 <gui.henriquess13@gmail.com>
2026-02-25 11:19:17 -0300

    refactor: folders structure

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,6 +1,6 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id"
-import { Answer } from "../entities/answer"
-import { AnswersRepository } from "../repositories/answers-repository"
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Answer } from '../../enterprise/entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
@@ -18,7 +18,7 @@ export class AnswerQuestionUseCase {
       questionId: new UniqueEntityId(questionId)
     })
 
-    await this.answersRepository.create(answer) 
+    await this.answersRepository.create(answer)
 
     return answer
   }
