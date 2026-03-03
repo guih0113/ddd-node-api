3cbc3abbcace3c17dfce1007efd7fb54db3193de
guih0113 <gui.henriquess13@gmail.com>
2026-03-03 11:35:35 -0300

    feat: implement delete answer comment use case with corresponding tests and factory method

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -2,4 +2,6 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 
 export interface AnswerCommentsRepository {
   create(answercomment: AnswerComment): Promise<void>
+  findById(id: string): Promise<AnswerComment | null>
+  delete(answercomment: AnswerComment): Promise<void>
 }
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -7,4 +7,20 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
+
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+
+    this.items.splice(itemIndex, 1)
+  }
 }
