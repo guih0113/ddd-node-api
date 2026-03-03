7f47aa63b96375196ffe4291027d6374647f9c71
guih0113 <gui.henriquess13@gmail.com>
2026-03-03 11:52:06 -0300

    feat: implement FetchQuestionComments use case with pagination support and corresponding tests

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
   create(questioncomment: QuestionComment): Promise<void>
   findById(id: string): Promise<QuestionComment | null>
+  findManyByQuestionId(questionId: string, params: PaginationParams): Promise<QuestionComment[]>
   delete(questioncomment: QuestionComment): Promise<void>
 }
